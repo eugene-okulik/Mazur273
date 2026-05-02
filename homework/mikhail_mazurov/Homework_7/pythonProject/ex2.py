6ff2b31b63c0dbbb07aa09f53b3f5bbe0f522ba5
Mikhail Mazurov <miiishamsk@gmail.com>
2026-05-03 02:00:52 +0400

    homework7 done

---
+++ homework/mikhail_mazurov/Homework_7/pythonProject/ex2.py
@@ -0,0 +1,10 @@
+words = {'I': 3, 'love': 5, 'Python': 1, '!': 50}
+
+def multipl_key(my_dict):
+    # print(my_dict.items())
+    for key, degree in my_dict.items():
+        print(key*degree)
+
+multipl_key(words)
+# стросил у жпт, оказывается можно указывать тип параметра и тогда нотационные методы типа .items будут подсказываться
+# проверю в след.задании
---
+++ homework/mikhail_mazurov/Homework_7/pythonProject/ex3.py
@@ -0,0 +1,12 @@
+dano1 = 'результат операции: 42'
+dano2 = 'результат операции: 514'
+dano3 = 'результат операции программы: 9'
+
+# так как индексом я уже делал, попробую split
+def plus10(my_string: str):
+    my_list = my_string.split()  #сработало то о чем писал в ex2, метод split был в подсказках
+    print(int(my_list[-1]) + 10)
+
+plus10(dano1)
+plus10(dano2)
+plus10(dano3)
