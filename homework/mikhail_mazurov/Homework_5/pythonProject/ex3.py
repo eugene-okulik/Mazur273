1b77fb152b5608b98ad21b57916a2c2f87bd357e
Mikhail Mazurov <miiishamsk@gmail.com>
2026-05-02 04:19:40 +0400

    homework5_replace_with_join

--- homework/mikhail_mazurov/Homework_5/pythonProject/ex3.py
+++ homework/mikhail_mazurov/Homework_5/pythonProject/ex3.py
@@ -1,5 +1,3 @@
 students = ['Ivanov', 'Petrov', 'Sidorov']
 subjects = ['math', 'biology', 'geography']
-st1, st2, st3 = students
-sub1, sub2, sub3 = subjects
-print(f'Students {st1}, {st2}, {st3} study these subjects: {sub1}, {sub2}, {sub3}')
+print('Students ' + ', '.join(students) + ' study these subjects: ' + ', '.join(subjects))
