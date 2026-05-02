835a971400d453ea8ccea5329f603ef522a6acb4
Mikhail Mazurov <miiishamsk@gmail.com>
2026-05-03 02:09:00 +0400

    homework7 done

--- homework/mikhail_mazurov/Homework_7/pythonProject/ex3.py
+++ homework/mikhail_mazurov/Homework_7/pythonProject/ex3.py
@@ -3,9 +3,12 @@ dano2 = 'результат операции: 514'
 dano3 = 'результат операции программы: 9'
 
 # так как индексом я уже делал, попробую split
+
+
 def plus10(my_string: str):
-    my_list = my_string.split()  #сработало то о чем писал в ex2, метод split был в подсказках
+    my_list = my_string.split()  # сработало то о чем писал в ex2, метод split был в подсказках
     print(int(my_list[-1]) + 10)
+
 
 plus10(dano1)
 plus10(dano2)
