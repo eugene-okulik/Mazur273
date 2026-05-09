7876c4d3bba78c4c85d088449a358df0f2ffa376
Mikhail Mazurov <miiishamsk@gmail.com>
2026-05-10 00:22:28 +0400

    homework12_v2

--- homework/mikhail_mazurov/Homework_12/pythonProject1/ex1.py
+++ homework/mikhail_mazurov/Homework_12/pythonProject1/ex1.py
@@ -10,7 +10,7 @@ def read_file(filename):
 
 
 class Flower:
-    def __init__(self, filename):
+    def __init__(self, filename, price):
         data = read_file(filename)
         self.lifetime = data.get('lifetime')
         self.privoz = datetime.datetime.strptime(
@@ -19,6 +19,7 @@ class Flower:
         ).date()
         self.color = data.get('color')
         self.freshness = (datetime.date.today() - self.privoz).days
+        self.price = price
 
     def __repr__(self):
         return f'{self.__class__.__name__}(color={self.color}, freshness={self.freshness} дней)'
@@ -26,17 +27,17 @@ class Flower:
 
 class Roses(Flower):
     def __init__(self, filename):
-        super().__init__(filename)
+        super().__init__(filename, 200)
 
 
 class Asters(Flower):
     def __init__(self, filename):
-        super().__init__(filename)
+        super().__init__(filename, 100)
 
 
 class Gerberas(Flower):
     def __init__(self, filename):
-        super().__init__(filename)
+        super().__init__(filename, 150)
 
 
 class Bouquet:
@@ -52,11 +53,25 @@ class Bouquet:
     def avg_freshness(self):
         return sum(x.freshness for x in self.flowers) / len(self.flowers)
 
+    def sum_price(self):
+        return sum(x.price for x in self.flowers)
+
+    def find_by(self, param, value):
+        result = [flower for flower in self.flowers
+                  if getattr(flower, param) == value]
+
+        if not result:
+            return f'Нет таких цветов у которых {param} == {value}'
+
+        return result
+
 
 rose = Roses('roses.txt')
 aster = Asters('asters.txt')
 gerbera = Gerberas('gerberas.txt')
 bouquet = Bouquet(rose, aster, gerbera)
 
+print(bouquet.find_by('color', 'white'))
 print(bouquet.avg_freshness())
 print(bouquet.sort_by_freshness())
+print(bouquet.sum_price())
